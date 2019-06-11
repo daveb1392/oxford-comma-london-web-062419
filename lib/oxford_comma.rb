faab6d9000a73dadbe7ac0fe4a7f0ae4c29dd8c0
github username <github email address>
2019-06-11 16:13:18 +0100

    oxford comma

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
-end
+    if array.length == 1 #if = to 1 element removing formating
+      return "#{array[0]}"
+    elsif array.length == 2 # if = to 2 elements, remove formating and add and.
+      return array.join(" and ")
+    elsif array.length >= 3
+      array[-1] = "and #{array[-1]}"  #if = to 3 elements, remove formatting return and + join ","
+      return array.join(", ")
+    end
+  end
